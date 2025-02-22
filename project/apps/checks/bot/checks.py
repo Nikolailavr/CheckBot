ad708ecaba33906a7cc0a1b0f955a4bf59d457ac
Nikolai <mail.@nlavrov.ru>
2025-02-22 13:01:35 +0700

    v1.01 Fix bag with place adress

--- project/apps/checks/bot/checks.py
+++ project/apps/checks/bot/checks.py
@@ -28,14 +28,14 @@ class Check:
         ticket = data[0].get("ticket", {}).get("document", {}).get("receipt", {})
         items = ticket.get("items", [])
         id_ = data[0].get("_id")
-        place = ticket.get("retailPlace", [])
-        place_adress = ticket.get("retailPlaceAddress", [])
-        iso_string = ticket.get("dateTime", [])
+        place = ticket.get("retailPlace", '')
+        place_adress = ticket.get("retailPlaceAddress", '')
+        iso_string = ticket.get("dateTime", '')
         date = datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S").strftime(date_format)
         for item in items:
             name = item.get("name", "Неизвестно")
             price = item.get("price", 0) / 100  # Цена в копейках
-            quantity = item.get("quantity", 1)
+            quantity = item.get("quantity", 0)
             total = item.get("sum", 0) / 100  # Итоговая сумма в копейках
             result.append([date, name, quantity, price, total, place, place_adress, id_])
         return result, id_
